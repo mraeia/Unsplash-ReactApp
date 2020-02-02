c46b247d6d89a4e226262773c863a7bd725a8d9a
Moustafa Aly <m.raeia@hotmail.com>
2020-02-02 17:02:26 -0500

    API request and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { chunk } from "lodash";
+import "./App.scss";
 
-function App() {
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(
+        `https://api.unsplash.com/search/photos/?page=0&per_page=30&query=car&client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
+      )
+      .then(data => {
+        let images = [];
+        if (data.data.results) {
+          data.data.results.forEach((result, index) => {
+            let image = {
+              index,
+              id: result.id,
+              smallURL: result.urls.small,
+              regularURL: result.urls.regular,
+              description: result.description
+            };
+            images.push(image);
+          });
+        }
+        setImages(images);
+        setLoading(false);
+      })
+      .catch(err => console.error(err));
+  }, []);
+
+  const renderColumn = array => {
+    return (
+      <div className="img-column">
+        {array.map(image => {
+          return <img key={image.id} src={image.smallURL} alt="result"></img>;
+        })}
+      </div>
+    );
+  };
+
+  const renderImages = images => {
+    let arrays = chunk(images, 10);
+    return (
+      <div className="flex-container">
+        {arrays.map((arr, index) => {
+          return (
+            <React.Fragment key={index}>{renderColumn(arr)}</React.Fragment>
+          );
+        })}
+      </div>
+    );
+  };
+
   return (
     <div>
-      Hello World
+      {images.length ? renderImages(images) : null}
+      {loading ? <div className="loader"></div> : null}
     </div>
   );
-}
+};
 
 export default App;
