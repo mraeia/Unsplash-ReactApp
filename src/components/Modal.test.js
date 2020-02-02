e26885a29584df7a6320618a33276427effade4c
Moustafa Aly <m.raeia@hotmail.com>
2020-02-02 18:24:44 -0500

    added unit tests

---
+++ src/components/Modal.test.js
@@ -0,0 +1,69 @@
+import Modal from "./Modal";
+import React from "react";
+import { shallow } from "enzyme";
+import checkPropTypes from "check-prop-types";
+
+const MOCK_PROPS = {
+  show: false,
+  toggleModal: jest.fn(),
+  currentImage: {
+    index: 0,
+    id: "Test",
+    smallURL:
+      "https://images.unsplash.com/photo-1493238792000-8113da705763?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjExMzA4NH0",
+    regularURL:
+      "https://images.unsplash.com/photo-1493238792000-8113da705763?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjExMzA4NH0",
+    descriptoon: "car"
+  },
+  images: [
+    {
+      index: 0,
+      id: "Test",
+      smallURL:
+        "https://images.unsplash.com/photo-1493238792000-8113da705763?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjExMzA4NH0",
+      regularURL:
+        "https://images.unsplash.com/photo-1493238792000-8113da705763?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjExMzA4NH0",
+      descriptoon: "car"
+    }
+  ],
+  setCurrentImage: jest.fn()
+};
+
+describe("Modal component", () => {
+  it("renders without crashing", () => {
+    const modal = document.createElement("div");
+    modal.setAttribute("id", "modal");
+    document.body.append(modal);
+    const component = shallow(<Modal />);
+    expect(component).not.toBeNull();
+  });
+  it("close modal click", () => {
+    const toggleModal = jest.fn();
+    const modal = document.createElement("div");
+    modal.setAttribute("id", "modal");
+    document.body.append(modal);
+    const component = shallow(<Modal show={true} toggleModal={toggleModal} />);
+    component.find(".modal.display-block").simulate("click");
+    expect(toggleModal).toHaveBeenCalledTimes(1);
+  });
+  it("switch images click", () => {
+    const modal = document.createElement("div");
+    modal.setAttribute("id", "modal");
+    document.body.append(modal);
+    const component = shallow(<Modal {...MOCK_PROPS} />);
+    component.find(".arrow-left").simulate("click", {
+      stopPropagation: jest.fn(),
+      target: { className: "arrow-left" }
+    });
+    expect(MOCK_PROPS.setCurrentImage).toHaveBeenCalled();
+  });
+  it("test props", () => {
+    const propsError = checkPropTypes(
+      Modal.propTypes,
+      MOCK_PROPS,
+      "props",
+      Modal.name
+    );
+    expect(propsError).toBeUndefined();
+  });
+});
