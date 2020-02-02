2ef85d50d9a95d6943c58e34a1d6c1ebdd62f5a4
Moustafa Aly <m.raeia@hotmail.com>
2020-02-02 17:23:29 -0500

    Added pagination

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { chunk } from "lodash";
+import ReactPaginate from "react-paginate";
 import "./App.scss";
 
 const App = () => {
   const [images, setImages] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setLoading(true);
     axios
       .get(
-        `https://api.unsplash.com/search/photos/?page=0&per_page=30&query=car&client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
+        `https://api.unsplash.com/search/photos/?page=${currentPage}&per_page=30&query=car&client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
       )
       .then(data => {
         let images = [];
@@ -27,11 +30,16 @@ const App = () => {
             images.push(image);
           });
         }
+        setPageCount(data.data.total_pages);
         setImages(images);
         setLoading(false);
       })
       .catch(err => console.error(err));
-  }, []);
+  }, [currentPage]);
+
+  const handlePageClick = e => {
+    setCurrentPage(e.selected + 1);
+  };
 
   const renderColumn = array => {
     return (
@@ -58,6 +66,21 @@ const App = () => {
 
   return (
     <div>
+      <ReactPaginate
+        previousLabel={"<"}
+        nextLabel={">"}
+        breakLabel={"..."}
+        breakClassName={"break-me"}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={8}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        subContainerClassName={"pages pagination"}
+        activeClassName={"active"}
+        previousClassName={"previous"}
+        nextClassName={"next"}
+      />
       {images.length ? renderImages(images) : null}
       {loading ? <div className="loader"></div> : null}
     </div>
